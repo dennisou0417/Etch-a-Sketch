4cccad6f61176aa1fcc42004e49a1a9bc9b6e934
dennisou0417 <dennis.ou0417@gmail.com>
2025-06-23 00:14:17 -0400

    adding comments to code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//Event Delegation on grid
 document.body.addEventListener("mouseover", function(event){
     if(event.target.matches(".grid-item")){
         event.target.classList.add("pen");
     }
 })
 
+//Creating Grid
 const container = document.querySelector("#container");
 let row = 16;
 let col = 16;
